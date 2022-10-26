97ec53950d91b489c0605ef611893cf4f70278bd
tempobrian <mr.penabrian@hotmail.com>
2022-10-26 12:01:21 -0400

    added pAequorFactory function

--- main.js
+++ main.js
@@ -13,7 +13,49 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
-
+const pAequorFactory = (num, arr) => {
+  return {
+    specimenNum: num,
+    dna: arr,
+    compareDNA(obj) {
+      let count = 0;
+      for (let i = 0; i < 15; i++) {
+        console.log(this.dna[i], obj.dna[i]);
+        if (this.dna[i] === obj.dna[i]) {
+          count++;
+        }
+      }
+      const percent = ((count / 15) * 100).toFixed(2);
+      return `specimen #${this.specimenNum} and specimen #${obj.specimenNum} have ${percent}% DNA in common`;
+    },
+    mutate() {
+      const dnaBase = ["A", "T", "C", "G"];
+      const randomNumber = Math.floor(Math.random() * 15);
+      const randomNumberInside = Math.floor(Math.random() * 3);
+      const randomDna = this.dna[randomNumber];
+      if (randomDna === "A") {
+        return (this.dna[randomNumber] = dnaBase.filter(
+          (item) => item !== randomDna
+        )[randomNumberInside]);
+      }
+      if (randomDna === "T") {
+        return (this.dna[randomNumber] = dnaBase.filter(
+          (item) => item !== randomDna
+        )[randomNumberInside]);
+      }
+      if (randomDna === "C") {
+        return (this.dna[randomNumber] = dnaBase.filter(
+          (item) => item !== randomDna
+        )[randomNumberInside]);
+      }
+      if (randomDna === "G") {
+        return (this.dna[randomNumber] = dnaBase.filter(
+          (item) => item !== randomDna
+        )[randomNumberInside]);
+      }
+    },
+  };
+};
 
 
 
